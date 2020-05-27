93e8451b58aea07591e1d9e512d1b62c67b9f3a4
K. Li <kangle.leehd@gmail.com>
2020-05-27 10:55:05 +0200

    code refactoring for loading data set

---
+++ PCA_LR_classification.py
@@ -0,0 +1,49 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+import matplotlib.pyplot as plt
+import numpy as np
+from load_dataset import load_data
+
+data_dir = '/home/kangle/dataset/PedBicCarData'
+train_data, train_label, test_data, test_label = load_data(data_dir, 2, 2)
+
+print('\nbegin PCA process.')
+pca = PCA(n_components=1000, svd_solver='randomized', whiten=True).fit(train_data)
+train_feature = pca.transform(train_data)
+print(train_feature.shape)
+
+print('\nbegin Logistic Regression.')
+lr = LogisticRegression(solver='lbfgs',multi_class='multinomial',max_iter=200)
+lr.fit(train_feature,train_label)
+
+print('\npredict for test data.')
+test_feature = pca.transform(test_data)
+print(test_feature.shape)
+test_pred = lr.predict(test_feature)
+train_pred = lr.predict(train_feature)
+
+print('\nevaluate the prediction(train data).')
+conf = confusion_matrix(train_label, train_pred)
+print(conf)
+score_precision = precision_score(train_label, train_pred, average=None)
+score_recall = recall_score(train_label, train_pred, average=None)
+print(score_precision)
+print(score_recall)
+
+print('\nevaluate the prediction(test data).')
+conf = confusion_matrix(test_label, test_pred)
+print(conf)
+score_precision = precision_score(test_label, test_pred, average=None)
+score_recall = recall_score(test_label, test_pred, average=None)
+print(score_precision)
+print(score_recall)
+
+plt.plot(pca.explained_variance_ratio_)
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.show()
+
+# param_grid = {'C': [1e3], 'gamma': [ 0.0001]}
+# svm = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+# svm = svm.fit(train_data, train_label)
+
---
+++ load_dataset.py
@@ -0,0 +1,52 @@
+from os.path import join as pjoin
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_file(index, type, rootDir):
+    file_index = "{0:02d}".format(index)
+    if index == 0:
+        file_name = type + 'NoCar' + '.h5'
+    else:
+        file_name = type + 'NoCar_' + file_index + '.h5'
+    file_name = pjoin(rootDir, file_name)
+    print(file_name)
+    f = h5py.File(file_name, 'r')
+    key = list(f.keys())[0]
+    data = np.array(f[key])
+    return data
+
+def load_data(rootDir, sampRateT, sampRateF):
+    print('load the data.')
+    train_data_raw = np.array([])
+    for i in range(1,21):
+        dataBlock = read_file(i, 'trainData', rootDir)
+        dataBlock = dataBlock[:,:,0::sampRateF,0::sampRateT]
+        train_data_raw = np.vstack([train_data_raw, dataBlock]) if train_data_raw.size else dataBlock
+
+    print(train_data_raw.shape)
+    imgplot = plt.imshow(train_data_raw[7, 0, :, :])
+    plt.show()
+    train_data = train_data_raw.reshape(train_data_raw.shape[0], -1)
+    print(train_data.shape)
+
+    test_data_raw = np.array([])
+    for i in range(1,6):
+        dataBlock = read_file(i, 'testData', rootDir)
+        dataBlock = dataBlock[:, :, 0::sampRateF, 0::sampRateT]
+        test_data_raw = np.vstack([test_data_raw, dataBlock]) if test_data_raw.size else dataBlock
+
+    print(test_data_raw.shape)
+    imgplot = plt.imshow(test_data_raw[7, 0, :, :])
+    plt.show()
+    test_data = test_data_raw.reshape(test_data_raw.shape[0], -1)
+    print(test_data.shape)
+
+    # read label
+    train_label = read_file(0, 'trainLabel', rootDir)
+    train_label = train_label.flatten()
+
+    test_label = read_file(0, 'testLabel', rootDir)
+    test_label = test_label.flatten()
+
+    return train_data, train_label, test_data, test_label
