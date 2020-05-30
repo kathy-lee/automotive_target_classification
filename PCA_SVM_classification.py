7e859348072926c4ea747a0cddc3795daf28fde2
K. Li <kangle.leehd@gmail.com>
2020-05-31 00:10:58 +0200

    add gridsearch svm parameters

--- PCA_SVM_classification.py
+++ PCA_SVM_classification.py
@@ -1,4 +1,4 @@
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.decomposition import PCA
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
 import matplotlib.pyplot as plt
@@ -16,7 +16,7 @@ train_data = scaler.fit_transform(train_data)
 test_data = scaler.transform(test_data)
 
 print('\nbegin PCA process.')
-pca = PCA(n_components=1000, svd_solver='randomized', whiten=True).fit(train_data)
+pca = PCA(n_components=30, svd_solver='randomized', whiten=True).fit(train_data)
 train_feature = pca.transform(train_data)
 print(train_feature.shape)
 
@@ -25,11 +25,14 @@ print('\nbegin svm.')
 #               'gamma': [0.0001, 0.001, 0.01, 0.1, 1]}
 param_grid = {'C': [1, 10],
               'gamma': [0.01, 0.1]}
-grid = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-grid.fit(train_data, train_label)
-print("The best parameters are %s with a score of %0.2f" % (grid.best_params_, grid.best_score_))
+svm = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+svm.fit(train_feature, train_label)
+print("The best parameters are %s with a score of %0.2f" % (svm.best_params_, svm.best_score_))
 
-svm = SVC(kernel='rbf', class_weight='balanced', **grid.best_params)
+#svm = SVC(kernel='rbf', class_weight='balanced', **grid.best_params)
+#svm = LinearSVC(C=1,loss='hinge')
+#svm = SVC(kernel='poly', gamma='scale')
+#svm.fit(train_feature,train_label)
 
 print('\npredict for test data.')
 test_feature = pca.transform(test_data)
@@ -57,7 +60,4 @@ plt.plot(pca.explained_variance_ratio_)
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.show()
 
-# param_grid = {'C': [1e3], 'gamma': [ 0.0001]}
-# svm = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-# svm = svm.fit(train_data, train_label)
 
