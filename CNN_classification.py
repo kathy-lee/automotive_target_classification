285081f3e781890cadeff02437b4e05cd9abc698
K. Li <kangle.leehd@gmail.com>
2020-07-07 10:42:52 +0200

    add training history plot

--- CNN_classification.py
+++ CNN_classification.py
@@ -1,33 +1,19 @@
 from tensorflow.keras import models
-from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout,BatchNormalization
 from tensorflow.keras.utils import to_categorical, normalize
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l1,l2
 import numpy as np
-from load_dataset import load_data
+from load_dataset import load_data, preprocess_data
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from time import process_time
+import matplotlib.pyplot as plt
+
 
 data_dir = '/home/kangle/dataset/PedBicCarData'
-train_data, train_label, test_data, test_label = load_data(data_dir, 1, 1)
-
-print("Data sample distribution in training set: %d %d %d %d %d\n" % (np.count_nonzero(train_label==1),
-      np.count_nonzero(train_label==2), np.count_nonzero(train_label==3),
-      np.count_nonzero(train_label==4), np.count_nonzero(train_label==5)))
-print("Data sample distribution in test set: %d %d %d %d %d\n" % (np.count_nonzero(test_label==1),
-      np.count_nonzero(test_label==2), np.count_nonzero(test_label==3),
-      np.count_nonzero(test_label==4), np.count_nonzero(test_label==5)))
-
-#train_data = normalize(train_data, axis=1)
-#test_data = normalize(test_data, axis=1)
-
-print(train_label[0:10])
-print(test_label[0:10])
-train_label = to_categorical(train_label-1, num_classes=5)
-test_label = to_categorical(test_label-1, num_classes=5)
-print(train_label.shape)
-print(test_label.shape)
-print(train_label[0:10,:])
-print(test_label[0:10,:])
+train_data, train_label, test_data, test_label = load_data(data_dir, 2, 2)
+train_data, train_label, test_data, test_label = preprocess_data(train_data, train_label, test_data, test_label, 'cnn')
 
 train_data, val_data, train_label, val_label = train_test_split(train_data, train_label, test_size=0.1, random_state=42)
 print("Split training data into training and validation data:\n")
@@ -35,31 +21,51 @@ print("training data: %d" % train_data.shape[0])
 print("validation data: %d" % val_data.shape[0])
 
 model = models.Sequential()
-model.add(Conv2D(6, [5, 5], input_shape=train_data.shape[1:], activation='relu', name='conv_1'))
+model.add(Conv2D(16, [5, 5], input_shape=train_data.shape[1:], activation='relu', kernel_initializer='he_uniform', kernel_regularizer='l2', name='conv_1'))
 model.add(MaxPooling2D())
-model.add(Conv2D(16, [5, 5], activation='relu', name='conv_2'))
+model.add(Conv2D(32, [5, 5], activation='relu', kernel_initializer='he_uniform', kernel_regularizer='l2', name='conv_2'))
 model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(120, activation='relu', name='dense_1'))
-model.add(Dropout(0.5))
-model.add(Dense(84, activation='relu', name='dense_2'))
-model.add(Dropout(0.5))
+model.add(Dense(120, activation='relu', kernel_initializer='he_uniform', name='dense_1'))
+#model.add(BatchNormalization())
+model.add(Dense(84, activation='relu', kernel_initializer='he_uniform', name='dense_2'))
+#model.add(BatchNormalization())
 model.add(Dense(5, activation='softmax', name='dense_3'))
 
-model.compile(optimizer='adam',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
+opt = Adam(learning_rate=0.01)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-results = model.fit(train_data,
+history = model.fit(train_data,
                     train_label,
-                    epochs=30,
+                    epochs=20,
                     batch_size=128,
                     verbose=2,
                     validation_data=(val_data, val_label))
 
 # evaluate model
+test_pred = model.predict(test_data)
+
 t_start = process_time()
 _,acc = model.evaluate(test_data, test_label, batch_size=128, verbose=2)
 t_end = process_time()
 t_cost = t_end - t_start
 print(f"Test Accuracy: {acc:.4f}, Inference time: {t_cost:.2f}s")
+
+plt.figure()
+plt.subplot(211)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend(['train', 'test'], loc='lower right')
+plt.grid(True)
+plt.subplot(212)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train', 'test'], loc='upper right')
+plt.grid(True)
+plt.suptitle('model accuracy and loss')
+plt.tight_layout()
+plt.show()
--- RNN_classification.py
+++ RNN_classification.py
@@ -4,25 +4,20 @@ from tensorflow.keras.utils import to_categorical, normalize
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import l1,l2
 import numpy as np
-from load_dataset import load_data
+from load_dataset import load_data, preprocess_data
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from time import process_time
-
+import matplotlib.pyplot as plt
 
 data_dir = '/home/kangle/dataset/PedBicCarData'
 train_data, train_label, test_data, test_label = load_data(data_dir, 2, 2)
+train_data, train_label, test_data, test_label = preprocess_data(train_data, train_label, test_data, test_label, 'rnn')
 
-train_data = np.squeeze(train_data)
-test_data = np.squeeze(test_data)
-train_data = np.transpose(train_data, (0,2,1))
-test_data = np.transpose(test_data, (0,2,1))
-print(train_data.shape)
-print(test_data.shape)
-print(train_label.shape)
-print(test_label.shape)
-print(train_label[0:10,:])
-print(test_label[0:10,:])
+# train_data = np.squeeze(train_data)
+# test_data = np.squeeze(test_data)
+# train_data = np.transpose(train_data, (0,2,1))
+# test_data = np.transpose(test_data, (0,2,1))
 
 train_data, val_data, train_label, val_label = train_test_split(train_data, train_label, test_size=0.1, random_state=42)
 print("Split training data into training and validation data:\n")
@@ -37,13 +32,11 @@ model.add(Dropout(0.2));
 model.add(Dense(5, activation='softmax'))
 
 opt = Adam(learning_rate=0.01)
-model.compile(optimizer='adam',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-results = model.fit(train_data,
+history = model.fit(train_data,
                     train_label,
-                    epochs=50,
+                    epochs=10,
                     batch_size=128,
                     verbose=2,
                     validation_data=(val_data, val_label))
@@ -56,3 +49,22 @@ _,acc = model.evaluate(test_data, test_label, batch_size=128, verbose=2)
 t_end = process_time()
 t_cost = t_end - t_start
 print(f"Test Accuracy: {acc:.4f}, Inference time: {t_cost:.2f}s")
+
+plt.figure()
+plt.subplot(211)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend(['train', 'test'], loc='lower right')
+plt.grid(True)
+plt.subplot(212)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['train', 'test'], loc='upper right')
+plt.grid(True)
+plt.suptitle('model accuracy and loss')
+plt.tight_layout()
+plt.show()
